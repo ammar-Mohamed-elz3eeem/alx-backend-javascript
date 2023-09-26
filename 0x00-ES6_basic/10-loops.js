1785dd4edc8785e19e735142ec39e5ab10662124
Ammar-Mohamed-Elz3eeem <ammarmohamedthez@gmail.com>
2023-09-26 03:02:59 +0200

    append to each array value

---
+++ 0x00-ES6_basic/10-loops.js
@@ -0,0 +1,9 @@
+export default function appendToEachArrayValue(array, appendString) {
+  const newArr = [];
+
+  for (const el of array) {
+    newArr.push(appendString + el);
+  }
+
+  return newArr;
+}
