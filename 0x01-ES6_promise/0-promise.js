df51ff4c8170162479ee176cdf941cbec6c5d772
Ammar-Mohamed-Elz3eeem <ammarmohamedthez@gmail.com>
2023-09-26 05:59:01 +0200

    finish promise learning

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,5 +1,5 @@
-function getResponseFromAPI() {
-  return new Promise((resolve, reject) => {
-    resolve("Hi");
+export default function getResponseFromAPI() {
+  return new Promise((resolve) => {
+    resolve('Hi');
   });
 }
