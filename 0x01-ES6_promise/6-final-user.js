25eb731104c7dbbfa683c1139ce5d6632113d2a3
Ammar-Mohamed-Elz3eeem <ammarmohamedthez@gmail.com>
2023-09-26 14:24:19 +0200

    fix linter issues

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,11 +1,12 @@
-import uploadPhoto from "./5-photo-reject";
-import signUpUser from "./4-user-promise";
+import uploadPhoto from './5-photo-reject';
+import signUpUser from './4-user-promise';
 
 export default function handleProfileSignup(fname, lname, filename) {
-  return Promise.allSettled([signUpUser(fname, lname), uploadPhoto(filename)]).then(res => (
-    res.map(o => ({
-      status: o.status,
-      value: o.status === "fulfilled" ? o.value : String(o.reason),
-    }))
-  ));
+  return Promise.allSettled([signUpUser(fname, lname), uploadPhoto(filename)])
+    .then((res) => (
+      res.map((obj) => ({
+        status: obj.status,
+        value: obj.status === 'fulfilled' ? obj.value : String(obj.reason),
+      }))
+    ));
 }
