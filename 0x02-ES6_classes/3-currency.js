850f626958b113b60e3de06950de818884d50c77
Ammar-Mohamed-Elz3eeem <ammarmohamedthez@gmail.com>
2023-09-29 02:35:23 +0200

    implement currency class

---
+++ 0x02-ES6_classes/3-currency.js
@@ -0,0 +1,38 @@
+export default class Currency {
+  constructor(code, name) {
+    if (typeof code !== 'string') {
+      throw new TypeError('Curreny code must be string');
+    }
+    if (typeof name !== 'string') {
+      throw new TypeError('Curreny name must be string');
+    }
+    this._code = code;
+    this._name = name;
+  }
+
+  get code() {
+    return this._code;
+  }
+
+  set code(ccode) {
+    if (typeof ccode !== 'string') {
+      throw new TypeError('Curreny code must be string');
+    }
+    this._code = ccode;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  set name(cname) {
+    if (typeof cname !== 'string') {
+      throw new TypeError('Curreny name must be string');
+    }
+    this._name = cname;
+  }
+
+  displayFullCurrency() {
+    return `${this._name} ${this._code}`;
+  }
+}
