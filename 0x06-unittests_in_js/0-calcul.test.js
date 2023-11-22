2ddcfa58687dde8e213703a1db02a77b92c827e3
Ammar-Mohamed-Elz3eeem <ammarmohamedthez@gmail.com>
2023-11-22 17:24:00 +0200

    import libraries

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -1,3 +1,6 @@
+const assert = require('assert');
+const calculateNumber = require('./0-calcul');
+
 describe('calculateNumber', () => {
   it('floating point whole numbers', () => {
     assert.strictEqual(calculateNumber(1.0, 2.0), 3);
