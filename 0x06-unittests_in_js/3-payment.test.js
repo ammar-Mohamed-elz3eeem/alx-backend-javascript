6673649bc6b3d2a70db64a88156f59652f371d87
Ammar-Mohamed-Elz3eeem <ammarmohamedthez@gmail.com>
2023-11-22 21:43:58 +0200

    sperate the test cases

--- 0x06-unittests_in_js/3-payment.test.js
+++ 0x06-unittests_in_js/3-payment.test.js
@@ -8,6 +8,12 @@ describe('sendPaymentRequestToApi', () => {
     let spy = sison.spy(Utils);
     sendPaymentRequestToApi(100, 20);
     expect(spy.calculateNumber.calledWith('SUM', 100, 20)).to.be.true;
+    spy.calculateNumber.restore();
+  });
+
+  it('check function is called once', () => {
+    let spy = sison.spy(Utils);
+    sendPaymentRequestToApi(100, 20);
     expect(spy.calculateNumber.callCount).to.be.equal(1);
     spy.calculateNumber.restore();
   });
