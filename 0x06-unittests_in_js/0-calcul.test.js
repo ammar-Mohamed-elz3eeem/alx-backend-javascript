0ecfbd7422784efd0c014226b3986bced0cb9652
Ammar-Mohamed-Elz3eeem <ammarmohamedthez@gmail.com>
2023-11-22 17:22:45 +0200

    test only foating points number

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -1,45 +1,33 @@
-const assert = require('assert');
-const calculateNumber = require('./0-calcul');
-
 describe('calculateNumber', () => {
   it('floating point whole numbers', () => {
-    assert.equal(calculateNumber(2.0, 3.0), 5);
-  })
-  
-  it('calculate is working with floats', () => {
-    assert.equal(calculateNumber(2, 3.4), 5);
+    assert.strictEqual(calculateNumber(1.0, 2.0), 3);
   });
 
-  it('calculate is working with integers', () => {
-    assert.equal(calculateNumber(2, 3), 5);
+  it('rounding down b\'s floating point fractional number', () => {
+    assert.strictEqual(calculateNumber(1.0, 2.4), 3);
   });
 
-
-  it('calculate is working with floats', () => {
-    assert.equal(calculateNumber(2, 3.7), 6);
+  it('rounding down a and b\'s floating point fractional number', () => {
+    assert.strictEqual(calculateNumber(1.4, 2.4), 3);
   });
 
-  it('working if both floats', () => {
-    assert.equal(calculateNumber(1.2, 3.7), 5);
+  it('rounding down a\'s floating point fractional number', () => {
+    assert.strictEqual(calculateNumber(1.4, 2.0), 3);
   });
 
-  it('working if both floats', () => {
-    assert.equal(calculateNumber(1.5, 3.7), 6);
+  it('rounding up b\'s floating point fractional numbers', () => {
+    assert.strictEqual(calculateNumber(1.0, 2.5), 4);
   });
 
-  it('working if one argument is not int or float', () => {
-    assert.equal(calculateNumber(1.5, "3.7"), 6);
+  it('rounding up a and b\'s floating point fractional numbers', () => {
+    assert.strictEqual(calculateNumber(2.6, 2.5), 6);
   });
 
-  it('working if both arguments is not int or float', () => {
-    assert.equal(calculateNumber("1.5", "3.7"), 6);
+  it('rounding up a\'s floating point fractional numbers', () => {
+    assert.strictEqual(calculateNumber(2.6, 2.0), 5);
   });
 
-  it('returning nan while rounding string litterals', () => {
-    assert.equal(calculateNumber("ali", 3.7), NaN);
-  });
-
-  it('returning nan while rounding arrays', () => {
-    assert.equal(calculateNumber([false], 3.7), NaN);
+  it('rounding down a and b floating point fractional numbers with trailing 9\'s', () => {
+    assert.strictEqual(calculateNumber(2.499999, 3.499999), 5);
   });
 });
