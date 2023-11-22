a9fd5a7845fce4d95fe825e3d6f8f275077f8560
Ammar-Mohamed-Elz3eeem <ammarmohamedthez@gmail.com>
2023-11-22 03:01:50 +0200

    add simple http server

---
+++ 0x05-Node_JS_basic/4-http.js
@@ -0,0 +1,12 @@
+const http = require('http');
+
+const app = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader("Content-Type", "text/plain");
+  res.write('Hello Holberton School!');
+  res.end();
+});
+
+app.listen(1245);
+
+module.exports = app;
