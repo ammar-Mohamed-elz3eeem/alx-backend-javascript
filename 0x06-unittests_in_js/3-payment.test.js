28bb478616b52cc98f705c4ecbd77a05dffb449a
Ammar-Mohamed-Elz3eeem <ammarmohamedthez@gmail.com>
2023-11-22 21:51:03 +0200

    fix linter issues

--- 0x06-unittests_in_js/3-payment.test.js
+++ 0x06-unittests_in_js/3-payment.test.js
@@ -5,14 +5,14 @@ const Utils = require('./utils');
 
 describe('sendPaymentRequestToApi', () => {
   it('check function called with specified parameters', () => {
-    let spy = sison.spy(Utils);
+    const spy = sison.spy(Utils);
     sendPaymentRequestToApi(100, 20);
     expect(spy.calculateNumber.calledWith('SUM', 100, 20)).to.be.true;
     spy.calculateNumber.restore();
   });
 
   it('check function is called once', () => {
-    let spy = sison.spy(Utils);
+    const spy = sison.spy(Utils);
     sendPaymentRequestToApi(100, 20);
     expect(spy.calculateNumber.callCount).to.be.equal(1);
     spy.calculateNumber.restore();
