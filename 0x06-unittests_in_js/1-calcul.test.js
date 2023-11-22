55a5d883b1f8802dc120b7f75123423fe6c78987
Ammar-Mohamed-Elz3eeem <ammarmohamedthez@gmail.com>
2023-11-22 18:44:56 +0200

    add more test cases

--- 0x06-unittests_in_js/1-calcul.test.js
+++ 0x06-unittests_in_js/1-calcul.test.js
@@ -6,24 +6,31 @@ describe('calculateNumber', () => {
     it('equal positive numbers', () => {
       assert.strictEqual(calculateNumber(2.0, 2.0, 'ADD'), 4);
     });
+
     it('equal positive numbers (one rounded to smallest & one rounded to biggest)', () => {
       assert.strictEqual(calculateNumber(2.7, 2.3, 'ADD'), 5);
     });
+
     it('one positive & one negative number', () => {
       assert.strictEqual(calculateNumber(-2.0, 2.0, 'ADD'), 0);
     });
+
     it('one positive rounded to smallest & one negative number rounded to biggest', () => {
       assert.strictEqual(calculateNumber(2.7, -2.3, 'ADD'), 1);
     });
+
     it('equal netive numbers', () => {
       assert.strictEqual(calculateNumber(-2.0, -2.0, 'ADD'), -4);
     });
+
     it('equal netive numbers (one rounded to smallest & one rounded to biggest)', () => {
       assert.strictEqual(calculateNumber(-2.7, -2.3, 'ADD'), -5);
     });
+
     it('0 + 0 = 0', () => {
       assert.strictEqual(calculateNumber(0.0, 0.0, 'ADD'), 0);
     });
+
     it('test with add lowercase', () => {
       assert.strictEqual(calculateNumber(0.0, 0.0, 'add'), 0);
     });
@@ -32,40 +39,78 @@ describe('calculateNumber', () => {
     it('equal positive numbers', () => {
       assert.strictEqual(calculateNumber(2.0, 2.0, 'SUBTRACT'), 0);
     });
+
     it('equal positive numbers (one rounded to smallest & one rounded to biggest)', () => {
       assert.strictEqual(calculateNumber(2.7, 2.3, 'SUBTRACT'), 1);
     });
+
     it('one positive & one negative number', () => {
       assert.strictEqual(calculateNumber(-2.0, 2.0, 'SUBTRACT'), -4);
     });
+
     it('one positive rounded to smallest & one negative number rounded to biggest', () => {
       assert.strictEqual(calculateNumber(2.7, -2.3, 'SUBTRACT'), 5);
     });
+
     it('equal netive numbers', () => {
       assert.strictEqual(calculateNumber(-2.0, -2.0, 'SUBTRACT'), 0);
     });
+
     it('equal netive numbers (one rounded to smallest & one rounded to biggest)', () => {
       assert.strictEqual(calculateNumber(-2.7, -2.3, 'SUBTRACT'), -1);
     });
+
     it('0 + 0 = 0', () => {
       assert.strictEqual(calculateNumber(0.0, 0.0, 'SUBTRACT'), 0);
     });
+
     it('test with subtract lowercase', () => {
       assert.strictEqual(calculateNumber(0.0, 0.0, 'subtract'), 0);
     });
   });
   describe('type = "DIVIDE"', () => {
+    it('equal positive numbers', () => {
+      assert.strictEqual(calculateNumber(3.0, 2.0, 'DIVIDE'), 1.5);
+    });
+
+    it('equal negative numbers', () => {
+      assert.strictEqual(calculateNumber(-3.0, -2.0, 'DIVIDE'), 1.5);
+    });
+
+    it('a negative & b positive numbers', () => {
+      assert.strictEqual(calculateNumber(-3.0, 2.0, 'DIVIDE'), -1.5);
+    });
+
+    it('a = 0 & b positive numbers', () => {
+      assert.strictEqual(calculateNumber(0.0, 2.0, 'DIVIDE'), 0);
+    });
+
+    it('a = 0 & b negative numbers', () => {
+      assert.strictEqual(calculateNumber(0.0, -2.0, 'DIVIDE'), -0);
+    });
+
+    it('b negative & a positive numbers', () => {
+      assert.strictEqual(calculateNumber(3.0, -2.0, 'DIVIDE'), -1.5);
+    });
+
     it('using lower case type will return 0', () => {
       assert.strictEqual(calculateNumber([false], 3.7, 'divide'), 0);
     });
+
     it('test should throw error if b == 0', () => {
       assert.strictEqual(calculateNumber(5.25, 0, 'DIVIDE'), 'Error');
     });
+
     it('test should throw error if rounded b == 0', () => {
       assert.strictEqual(calculateNumber(5.25, 0.49, 'DIVIDE'), 'Error');
     });
+
     it('test shouldn\'t throw error if rounded b > 0', () => {
       assert.notStrictEqual(calculateNumber(5.25, 0.51, 'DIVIDE'), 'Error');
+    });
+
+    it('0 / 0 = "Error"', () => {
+      assert.strictEqual(calculateNumber(0.0, 0.0, 'DIVIDE'), 'Error');
     });
   });
 });
