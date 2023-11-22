6787b41f23025de7f2a6c7f3095f1ea227be6f59
Ammar-Mohamed-Elz3eeem <ammarmohamedthez@gmail.com>
2023-11-22 21:31:48 +0200

    add restore method

--- 0x06-unittests_in_js/3-payment.test.js
+++ 0x06-unittests_in_js/3-payment.test.js
@@ -4,18 +4,21 @@ const sendPaymentRequestToApi = require('./3-payment');
 const Utils = require('./utils');
 
 describe('sendPaymentRequestToApi same logic as calculateNumber', () => {
-  let spy;
-
+  
   before(() => {
-    spy = sison.spy(Utils);
-    sendPaymentRequestToApi(100, 20);
   });
-
+  
   it('check function called with specified parameters', () => {
+    let spy = sison.spy(Utils);
+    sendPaymentRequestToApi(100, 20);
     expect(spy.calculateNumber.calledWith('SUM', 100, 20)).to.be.true;
+    spy.calculateNumber.restore();
   });
-
+  
   it('check function is called once', () => {
+    let spy = sison.spy(Utils);
+    sendPaymentRequestToApi(100, 20);
     expect(spy.calculateNumber.callCount).to.be.equal(1);
+    spy.calculateNumber.restore();
   });
 });
