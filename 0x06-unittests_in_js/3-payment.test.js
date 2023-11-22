891d2e0361ed4baa7810b1aee375f449902b2718
Ammar-Mohamed-Elz3eeem <ammarmohamedthez@gmail.com>
2023-11-22 21:33:37 +0200

    add two tests in same it

--- 0x06-unittests_in_js/3-payment.test.js
+++ 0x06-unittests_in_js/3-payment.test.js
@@ -4,20 +4,10 @@ const sendPaymentRequestToApi = require('./3-payment');
 const Utils = require('./utils');
 
 describe('sendPaymentRequestToApi same logic as calculateNumber', () => {
-  
-  before(() => {
-  });
-  
   it('check function called with specified parameters', () => {
     let spy = sison.spy(Utils);
     sendPaymentRequestToApi(100, 20);
     expect(spy.calculateNumber.calledWith('SUM', 100, 20)).to.be.true;
-    spy.calculateNumber.restore();
-  });
-  
-  it('check function is called once', () => {
-    let spy = sison.spy(Utils);
-    sendPaymentRequestToApi(100, 20);
     expect(spy.calculateNumber.callCount).to.be.equal(1);
     spy.calculateNumber.restore();
   });
