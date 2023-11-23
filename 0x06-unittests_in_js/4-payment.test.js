fce9c6a8f0e55bb8c588e63aa66c6abf5e9d2306
Ammar-Mohamed-Elz3eeem <ammarmohamedthez@gmail.com>
2023-11-23 12:05:01 +0200

    remove afterEach hook

--- 0x06-unittests_in_js/4-payment.test.js
+++ 0x06-unittests_in_js/4-payment.test.js
@@ -4,10 +4,6 @@ const sendPaymentRequestToApi = require('./4-payment');
 const Utils = require('./utils');
 
 describe('sendPaymentRequestToApi', () => {
-  afterEach(() => {
-    sison.restore();
-  });
-
   it('change impl of sendPaymentRequestToApi', () => {
     const stub = sison.stub(Utils, "calculateNumber").returns(10);
     const spy = sison.spy(console);
@@ -15,5 +11,7 @@ describe('sendPaymentRequestToApi', () => {
     expect(stub.calledWith('SUM', 100, 20)).to.be.true;
     expect(spy.log.callCount).to.equal(1);
     expect(spy.log.calledWith('The total is: 10')).to.be.true;
+    stub.restore();
+    spy.log.restore();
   });
 });
