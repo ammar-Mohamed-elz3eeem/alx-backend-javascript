41bd16f370e1930877d9f81bd8c88590f77de4ca
Ammar-Mohamed-Elz3eeem <ammarmohamedthez@gmail.com>
2023-11-23 11:42:13 +0200

    test using stubs

---
+++ 0x06-unittests_in_js/4-payment.test.js
@@ -0,0 +1,19 @@
+const sison = require('sinon');
+const { expect } = require('chai');
+const sendPaymentRequestToApi = require('./4-payment');
+const Utils = require('./utils');
+
+describe('sendPaymentRequestToApi', () => {
+  afterEach(() => {
+    sison.restore();
+  });
+
+  it('change impl of sendPaymentRequestToApi', () => {
+    const stub = sison.stub(Utils, "calculateNumber").returns(10);
+    const spy = sison.spy(console);
+    sendPaymentRequestToApi(100, 20);
+    expect(stub.calledWith('SUM', 100, 20)).to.be.true;
+    expect(spy.log.callCount).to.equal(1);
+    expect(spy.log.calledWith('The total is: 10')).to.be.true;
+  });
+});
